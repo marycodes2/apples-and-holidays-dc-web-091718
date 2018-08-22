b7770d7a8c5244e55a30c5d792249381461c5a29
Mary  Beliveau <mary.e.beliveau@gmail.com>
2018-08-22 15:45:17 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -94,7 +94,7 @@ def all_supplies_in_holidays(holiday_hash)
       item_list.each do |item|
         items_array << item.capitalize
       end 
-      puts items_array
+      puts items_array.join(", ")
       #items_array = items_array.join(", ")
         #puts "  #{holiday.to_s.capitalize}: #{items_array}"
       end
